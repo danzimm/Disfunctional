6110b1b909f8abf8ba781cd311a20e69b1bb66f2
Dan Zimmerman <daniel.zimmerman@me.com>
2015-04-13 14:45:41 -0500

    Initial Commit

---
+++ Disfunctional/Disfunctional.h
@@ -0,0 +1,19 @@
+//
+//  Disfunctional.h
+//  Disfunctional
+//
+//  Created by Dan Zimmerman on 4/13/15.
+//  Copyright (c) 2015 Dan Zimmerman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Disfunctional.
+FOUNDATION_EXPORT double DisfunctionalVersionNumber;
+
+//! Project version string for Disfunctional.
+FOUNDATION_EXPORT const unsigned char DisfunctionalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Disfunctional/PublicHeader.h>
+
+
